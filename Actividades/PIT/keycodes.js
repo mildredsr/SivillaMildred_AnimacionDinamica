a8f3f38fb5c9cc138fec5559e2acd8768ed6803c
Mildred Sivilla <mildredsr@cetys.edu.mx>
2026-09-07 23:06:39 -0700

    Add files via upload

---
+++ Actividades/PIT/keycodes.js
@@ -0,0 +1,19 @@
+var keycode = {
+//arrows
+ LEFT: 37,
+ UP: 38,
+ RIGHT: 39,
+ DOWN: 40,
+
+ SPACE: 32,
+ ENTER: 13,
+
+ //letras y num.
+    A: 65,
+    B: 66,
+    C: 67,
+    D: 68,
+    E: 69,
+    F: 70,
+    
+};
